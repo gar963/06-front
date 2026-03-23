586c74ec37f5465b482b54e512d723bfdbae9d19
GABRIEL RENATO NASCIMENTO MANZOLLA <25203680@CFP127ALUNO.LOCAL>
2026-03-23 16:53:43 -0300

    impar e par

---
+++ 05-contador/cont.js
@@ -0,0 +1,45 @@
+
+
+function numberlist(quantidade){
+    let listanumeros=[]
+
+    for (let i = 1; i <= quantidade; i++) {
+        listanumeros.push(i)
+    }
+    console.log( listanumeros)
+}
+
+function listapar(quantidade) {
+    let pares = []
+    
+    
+    for (let i = 1; i <= quantidade  ; i++) {
+        
+            pares.push(i * 2)
+         
+    }
+    console.log(pares)
+    
+}
+
+function listimpar(quantidade) {
+    let impar =[]
+    for (let i = 1; i <= quantidade ; i++) {
+        
+            impar.push(i * 2 - 1 )
+        
+        
+    }
+         
+    
+    console.log(impar)
+}
+
+function criarlinha(params) {
+    
+}
+
+
+numberlist(5)
+listapar(5)
+listimpar(5)
